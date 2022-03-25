5140bd0f2c519e1010325e3269a5fb3b98383bcb
gentlekboy <gentlekboy@gmail.com>
2022-03-25 16:40:40 +0100

    untracking jni

--- app/src/main/java/com/gentlekboy/openweatherapp/ui/MainActivity.kt
+++ app/src/main/java/com/gentlekboy/openweatherapp/ui/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.gentlekboy.openweatherapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.gentlekboy.openweatherapp.R
 
 class MainActivity : AppCompatActivity() {
+
+    init {
+        System.loadLibrary("api-keys")
+    }
+
+    private external fun getKeys() : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("GKB", "HIDDEN API-KEY -> ${getKeys()}")
     }
 }
