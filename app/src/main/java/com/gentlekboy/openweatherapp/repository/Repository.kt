a1cfe70d460e672fd65933019a48590cea165dd1
gentlekboy <gentlekboy@gmail.com>
2022-03-26 19:55:55 +0100

    manipulated data from the API into room

--- app/src/main/java/com/gentlekboy/openweatherapp/repository/Repository.kt
+++ app/src/main/java/com/gentlekboy/openweatherapp/repository/Repository.kt
@@ -1,27 +1,69 @@
 package com.gentlekboy.openweatherapp.repository
 
-import com.gentlekboy.openweatherapp.data.model.city.CityWeatherResponse
+import android.util.Log
+import com.gentlekboy.openweatherapp.data.database.dao.CoordinatesDao
+import com.gentlekboy.openweatherapp.data.database.dao.WeatherDao
 import com.gentlekboy.openweatherapp.data.network.ApiInterface
-import retrofit2.Response
 import javax.inject.Inject
 
 class Repository @Inject constructor(
-    private val apiInterface: ApiInterface
+    private val apiInterface: ApiInterface,
+    private val coordinatesDao: CoordinatesDao,
+    private val weatherDao: WeatherDao
 ) : RepositoryInterface {
 
-    override suspend fun getWeatherByCity(
+    override suspend fun fetchCoordinatesFromApiToDb(
         cityName: String,
         apiKey: String
-    ): Response<CityWeatherResponse> =
-        apiInterface.getWeatherReportByCityName(cityName, apiKey)
+    ) {
+        try {
+            val response = apiInterface.fetchCoordinatesByCityName(cityName, apiKey)
+
+            if (response.isSuccessful) {
+                val data = response.body()
+
+                if (data != null) coordinatesDao.insertCity(response.body()!!)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("GKB", "fetchCoordinatesFromApiToDb catch block in repository: ${e.message}")
+        }
+    }
+
+    override suspend fun fetchWeatherFromApiToDb(
+        latitude: Double,
+        longitude: Double,
+        apiKey: String
+    ) {
+        try {
+            val response = apiInterface.fetchWeatherReportByCoordinates(
+                latitude = latitude,
+                longitude = longitude,
+                apiKey = apiKey
+            )
+
+            if (response.isSuccessful) {
+                val data = response.body()
+
+                if (data != null) weatherDao.insertWeatherData(response.body()!!)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.d("GKB", "fetchWeatherFromApiToDb catch block in repository: ${e.message}")
+        }
+    }
 
     override suspend fun getWeatherByCoordinates(
         latitude: Double,
         longitude: Double,
         apiKey: String
-    ) = apiInterface.getWeatherReportByCoordinates(
+    ) = apiInterface.fetchWeatherReportByCoordinates(
         latitude = latitude,
         longitude = longitude,
         apiKey = apiKey
     )
+
+    override fun getCoordinatesFromDb() = coordinatesDao.fetchCityInfo()
+
+    override fun getWeatherFromDb() = weatherDao.fetchWeatherData()
 }
