76685ab29af5245e0c7427556777816f8d229c84
gentlekboy <gentlekboy@gmail.com>
2022-03-26 19:54:25 +0100

    injected dao

--- app/src/main/java/com/gentlekboy/openweatherapp/di/RepositoryModule.kt
+++ app/src/main/java/com/gentlekboy/openweatherapp/di/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.gentlekboy.openweatherapp.di
 
+import com.gentlekboy.openweatherapp.data.database.dao.CoordinatesDao
+import com.gentlekboy.openweatherapp.data.database.dao.WeatherDao
 import com.gentlekboy.openweatherapp.data.network.ApiInterface
 import com.gentlekboy.openweatherapp.repository.Repository
 import com.gentlekboy.openweatherapp.repository.RepositoryInterface
@@ -15,6 +17,11 @@ class RepositoryModule {
 
     @Singleton
     @Provides
-    fun provideRepository(apiInterface: ApiInterface): RepositoryInterface =
-        Repository(apiInterface)
+    fun provideRepository(
+        apiInterface: ApiInterface,
+        coordinatesDao: CoordinatesDao,
+        weatherDao: WeatherDao
+    ): RepositoryInterface {
+        return Repository(apiInterface, coordinatesDao, weatherDao)
+    }
 }
