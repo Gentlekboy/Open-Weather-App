c02068c5e8175fa5800705e71832592c1db4293d
gentlekboy <gentlekboy@gmail.com>
2022-03-27 22:36:21 +0100

    added setAsFavourite method

--- app/src/main/java/com/gentlekboy/openweatherapp/utils/clickinterface/RecyclerviewClickInterface.kt
+++ app/src/main/java/com/gentlekboy/openweatherapp/utils/clickinterface/RecyclerviewClickInterface.kt
@@ -2,4 +2,5 @@ package com.gentlekboy.openweatherapp.utils.clickinterface
 
 interface RecyclerviewClickInterface {
     fun navigateToCityDetails(position: Int)
+    fun setAsFavourite(position: Int)
 }
