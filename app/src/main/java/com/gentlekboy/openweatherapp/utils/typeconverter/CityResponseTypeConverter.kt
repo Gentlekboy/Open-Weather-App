bb9c9eae0f928ff81920d5142049ca31e2790227
gentlekboy <gentlekboy@gmail.com>
2022-03-27 22:33:39 +0100

    updated city response type converter

--- app/src/main/java/com/gentlekboy/openweatherapp/utils/typeconverter/CityResponseTypeConverter.kt
+++ app/src/main/java/com/gentlekboy/openweatherapp/utils/typeconverter/CityResponseTypeConverter.kt
@@ -1,9 +1,9 @@
 package com.gentlekboy.openweatherapp.utils.typeconverter
 
 import androidx.room.TypeConverter
-import com.gentlekboy.openweatherapp.data.model.cityresponse.Coord
-import com.gentlekboy.openweatherapp.data.model.cityresponse.Sys
+import com.gentlekboy.openweatherapp.data.model.cityresponse.*
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class CityResponseTypeConverter {
 
@@ -18,4 +18,31 @@ class CityResponseTypeConverter {
 
     @TypeConverter
     fun fromStringToSys(sys: String): Sys = Gson().fromJson(sys, Sys::class.java)
+
+    @TypeConverter
+    fun fromCloudsToString(clouds: Clouds): String = Gson().toJson(clouds)
+
+    @TypeConverter
+    fun fromStringToClouds(clouds: String): Clouds = Gson().fromJson(clouds, Clouds::class.java)
+
+    @TypeConverter
+    fun fromMainToString(main: Main): String = Gson().toJson(main)
+
+    @TypeConverter
+    fun fromStringToMain(main: String): Main = Gson().fromJson(main, Main::class.java)
+
+    @TypeConverter
+    fun fromWindToString(wind: Wind): String = Gson().toJson(wind)
+
+    @TypeConverter
+    fun fromStringToWind(wind: String): Wind = Gson().fromJson(wind, Wind::class.java)
+
+    @TypeConverter
+    fun fromWeatherToString(weather: List<Weather>?): String = Gson().toJson(weather)
+
+    @TypeConverter
+    fun fromStringToWeather(weather: String): List<Weather>? {
+        val listOfWeather = object : TypeToken<List<Weather>?>() {}.type
+        return Gson().fromJson(weather, listOfWeather)
+    }
 }
