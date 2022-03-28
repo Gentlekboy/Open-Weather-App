ea99d650a3a790b9599342672b4d237e98f64c9f
gentlekboy <gentlekboy@gmail.com>
2022-03-28 12:59:45 +0100

    added extension function to determine internet connectivity

--- app/src/main/java/com/gentlekboy/openweatherapp/utils/Extensions.kt
+++ app/src/main/java/com/gentlekboy/openweatherapp/utils/Extensions.kt
@@ -1,5 +1,9 @@
 package com.gentlekboy.openweatherapp.utils
 
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -15,4 +19,35 @@ fun Int.convertTimeStampToTime(): String {
     val simpleDateFormat = SimpleDateFormat("h:mm a", Locale.ENGLISH)
 
     return simpleDateFormat.format(date)
+}
+
+fun Context.isInternetAvailable(): Boolean {
+    var result = false
+    val connectivityManager =
+        this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val networkCapabilities = connectivityManager.activeNetwork ?: return false
+        val actNw =
+            connectivityManager.getNetworkCapabilities(networkCapabilities) ?: return false
+        result = when {
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+            actNw.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+            else -> false
+        }
+    } else {
+        connectivityManager.run {
+            connectivityManager.activeNetworkInfo?.run {
+                result = when (type) {
+                    ConnectivityManager.TYPE_WIFI -> true
+                    ConnectivityManager.TYPE_MOBILE -> true
+                    ConnectivityManager.TYPE_ETHERNET -> true
+                    else -> false
+                }
+
+            }
+        }
+    }
+
+    return result
 }
